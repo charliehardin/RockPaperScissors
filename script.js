874f1e0c4f49985368811d85372e4eae6736e345
Charlie Hardin <106214877+charliehardin@users.noreply.github.com>
2023-03-23 20:30:52 -0600

    Finished adding base game functionality

--- script.js
+++ script.js
@@ -16,17 +16,42 @@ function playGame (playerSelection, computerSelection) {
     const rockBeatsScissors = "Rock Beats Scissors!";
     const scissorsBeatsPaper = "Scissors Beats Paper!"
     const tie = "Tie!"
+    const invalid = "Please enter a valid choice."
     if (playerSelection.toLowerCase() == "rock" && computerSelection == "Paper") {
         return paperBeatsRock;
-    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == "Scissors") {
+    } 
+    else if (playerSelection.toLowerCase() == 'rock' && computerSelection == "Scissors") {
         return rockBeatsScissors;
-    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'Scissors'){
+    } 
+    else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'Rock'){
+        return tie; 
+    }
+    else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'Scissors'){
         return scissorsBeatsPaper;
-    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'Rock'){
+    }
+    else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'Rock'){
+        return paperBeatsRock;
+    }
+    else if (playerSelection.toLowerCase() == 'paper' && computerSelection == "Paper"){
         return tie;
     }
+    else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == "Rock"){
+        return rockBeatsScissors;
+    }
+    else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == "Paper"){
+        return scissorsBeatsPaper;
+    }
+    else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == "Scissors"){
+        return tie;
+    }
+    else {
+            return invalid;
+        }
 }
 
+
 console.log(playGame(playerSelection, computerSelection));
+
+
 
 
