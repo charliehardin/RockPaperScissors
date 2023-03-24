bde420134c7a23fa42ad92b10d07dc770c3ed031
Charlie Hardin <106214877+charliehardin@users.noreply.github.com>
2023-03-24 17:44:03 -0600

    Added score counter and fixed loop for gameplay

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 // Rock Paper Scissors Game (Console Version)
 const choices = ['Rock', 'Paper', 'Scissors'];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice (choices) {
     const randomChoice = Math.floor(Math.random() * choices.length);
     return choices[randomChoice];
 }
 
-const computerChoice = getComputerChoice(choices);
-let computerSelection = computerChoice; 
-console.log(`Computer Has Chosen ${computerChoice}`);
+const rounds = [1, 2, 3, 4, 5];
 
-const playerSelection = prompt("Enter Your Choice:");
+rounds.forEach(() => {
+    const computerChoice = getComputerChoice(choices);
+    let computerSelection = computerChoice;
+    console.log(`Computer Has Chosen ${computerChoice}`);
+    const playerSelection = prompt("Enter Your Choice:");
+    const result = playGame(playerSelection, computerSelection);
+    console.log(result);
+    if (result.startsWith("You win!")) {
+        playerScore++;
+    } else if (result.startsWith("You lose!")) {
+        computerScore++;
+    }
+});
+
+console.log(`Player Score: ${playerScore}`);
+console.log(`Computer Score: ${computerScore}`);
+
+
 
 function playGame (playerSelection, computerSelection) {
     const computerWins = `You lose! ${computerSelection} beats ${playerSelection}`;
@@ -49,22 +66,6 @@ function playGame (playerSelection, computerSelection) {
             return invalid;
         }
 }
-
-
-// console.log(playGame(playerSelection, computerSelection));
-
-
-const rounds = [1, 2, 3, 4, 5];
-
-rounds.forEach(() => {
-    const playerSelection = prompt("Enter Your Choice:");
-    const computerSelection = getComputerChoice(choices);
-    console.log(`Computer Has Chosen ${computerSelection}`);
-    console.log(playGame(playerSelection, computerSelection));
-  });
-
-  
-
 
 
 
