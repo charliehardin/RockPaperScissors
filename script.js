dc21d1eac2b227633ae212ee41db5c4fdff97be1
Charlie Hardin <106214877+charliehardin@users.noreply.github.com>
2023-03-24 17:19:15 -0600

    Finalized basic functionality of main game

--- script.js
+++ script.js
@@ -5,41 +5,42 @@ function getComputerChoice (choices) {
     const randomChoice = Math.floor(Math.random() * choices.length);
     return choices[randomChoice];
 }
-const computerChoice = getComputerChoice(choices);
-console.log(computerChoice);
 
-let playerSelection = prompt("Enter Your Choice: ")
-let computerSelection = computerChoice;
+const computerChoice = getComputerChoice(choices);
+let computerSelection = computerChoice; 
+console.log(`Computer Has Chosen ${computerChoice}`);
+
+const playerSelection = prompt("Enter Your Choice:");
 
 function playGame (playerSelection, computerSelection) {
-    const paperBeatsRock = "Paper Beats Rock!";
-    const rockBeatsScissors = "Rock Beats Scissors!";
-    const scissorsBeatsPaper = "Scissors Beats Paper!"
+    const computerWins = `You lose! ${computerSelection} beats ${playerSelection}`;
+    const playerWins = `You win! ${playerSelection} beats ${computerSelection}`;
     const tie = "Tie!"
     const invalid = "Please enter a valid choice."
+
     if (playerSelection.toLowerCase() == "rock" && computerSelection == "Paper") {
-        return paperBeatsRock;
+        return computerWins;
     } 
     else if (playerSelection.toLowerCase() == 'rock' && computerSelection == "Scissors") {
-        return rockBeatsScissors;
+        return playerWins;
     } 
     else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'Rock'){
         return tie; 
     }
     else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'Scissors'){
-        return scissorsBeatsPaper;
+        return computerWins;
     }
     else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'Rock'){
-        return paperBeatsRock;
+        return playerWins;
     }
     else if (playerSelection.toLowerCase() == 'paper' && computerSelection == "Paper"){
         return tie;
     }
     else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == "Rock"){
-        return rockBeatsScissors;
+        return computerWins;
     }
     else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == "Paper"){
-        return scissorsBeatsPaper;
+        return playerWins;
     }
     else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == "Scissors"){
         return tie;
@@ -51,6 +52,11 @@ function playGame (playerSelection, computerSelection) {
 
 
 console.log(playGame(playerSelection, computerSelection));
+
+
+
+
+
 
 
 
